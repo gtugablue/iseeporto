0225c668b5fc544e18339708e5f1fb3b99474dda
RevTut <joaosilva@revtut.net>
2015-09-08 14:29:17 +0100

    Trying to change queries to prepared statements.

--- Web/api/api.php
+++ Web/api/api.php
@@ -9,11 +9,39 @@
 require_once "../includes/config.php";
 require_once "../includes/db_connect.php";
 
+function db_get_test() {
+    $numArgs=func_num_args();
+    if($numArgs < 1)
+        return null;
+
+    // Get arguments
+    $type = '';
+    $argList=func_get_args();
+    $query = (string) $argList[0];
+    $queryArgs = array();
+    for($i=1;$i<$numArgs;$i++) {
+        $type = $type . 's';
+        $queryArgs[$i]=$argList[$i];
+    }
+
+    // database query
+    global $db_connection;
+    echo $db_connection->get_connection_stats();
+
+    $prepStatement = $db_connection->prepare($query);
+    $prepStatement->bind_param($type, $queryArgs);
+    $result = $prepStatement->execute();
+
+    if (!$result || mysqli_num_rows($result) == 0) return null;
+
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+    return array_map("utf8_encode", $row);
+}
+
 function db_get($query)
 {
     global $db_connection;
-
-    $PoI_info = array();
 
     $result = mysqli_query($db_connection, $query);
 
@@ -26,8 +54,8 @@ function db_get($query)
 
 function get_PoI_info($id)
 {
-    $sql = "SELECT typeId, regionId, name, description, latitude, longitude FROM PointsOfInterest WHERE id = ".$id;
-    return db_get($sql);
+    $sql = "SELECT typeId, regionId, name, description, latitude, longitude FROM PointsOfInterest WHERE id = ?";
+    return db_get_test($sql, $id);
 }
 
 function get_reviews($id)
@@ -50,7 +78,7 @@ if (isset($_GET["action"]))
             break;
         case "get_poi_info":
             if (isset($_GET["id"]))
-                $value = get_PoI_info(mysqli_real_escape_string($db_connection, $_GET["id"]));
+                $value = get_PoI_info($_GET["id"]);
             else
                 $value = "Missing argument";
             break;
