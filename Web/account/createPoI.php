1d534d5c75f47fa4d1d9bca9007b27a63abf470c
gtugablue <gtugablue@gmail.com>
2015-09-09 11:40:31 +0100

    Fixed encodings.

--- Web/account/createPoI.php
+++ Web/account/createPoI.php
@@ -13,8 +13,7 @@ $userNode = getFacebookGraphUser($fb, $_SESSION['facebook_access_token']);
 <html lang="en">
 
 <head>
-
-    <!--<meta charset="utf-8">-->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
@@ -112,7 +111,7 @@ $userNode = getFacebookGraphUser($fb, $_SESSION['facebook_access_token']);
                             </div>
 
                             <div class="form-group">
-                                <label>Descrição</label>
+                                <label>DescriÃ§Ã£o</label>
                                 <textarea class="form-control" rows="3"></textarea>
                             </div>
 
--- Web/account/myPoIs.php
+++ Web/account/myPoIs.php
@@ -13,8 +13,7 @@ $userNode = getFacebookGraphUser($fb, $_SESSION['facebook_access_token']);
 <html lang="en">
 
 <head>
-
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
