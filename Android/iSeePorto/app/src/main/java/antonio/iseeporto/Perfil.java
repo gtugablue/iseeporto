58ad1ddf9e26faaecf39931660a94ff7c55f848e
António Ramadas <up201303568@gcloud.fe.up.pt>
2015-09-09 17:43:50 +0100

    Ping information from the server

--- Android/iSeePorto/app/src/main/java/antonio/iseeporto/Perfil.java
+++ Android/iSeePorto/app/src/main/java/antonio/iseeporto/Perfil.java
@@ -2,31 +2,114 @@ package antonio.iseeporto;
 
 import android.app.Fragment;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
-import android.graphics.RectF;
-import android.graphics.Bitmap.Config;
-import android.graphics.PorterDuff.Mode;
-import android.media.Image;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Random;
+import java.util.Vector;
 
 /**
  * Created by Antonio on 08-09-2015.
  */
 public class Perfil extends Fragment {
 
+    String rec;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.perfil, container, false);
+        View view = inflater.inflate(R.layout.perfil, container, false);
+        AsyncTask task = new JSONAsyncTask().execute("http://iseeporto.revtut.net/api/api.php?action=get_poi_info&id=1");
+        return view;
+    }
+
+    class JSONAsyncTask extends AsyncTask<String, Void, Boolean> {
+
+        ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(getActivity());
+            dialog.setMessage("Loading, please wait");
+            dialog.setTitle("Connecting server");
+            dialog.show();
+            dialog.setCancelable(false);
+        }
+
+        @Override
+        protected Boolean doInBackground(String... urls) {
+            try {
+
+                //------------------>>
+                HttpGet httppost = new HttpGet(urls[0]);
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse response = httpclient.execute(httppost);
+
+                // StatusLine stat = response.getStatusLine();
+                int status = response.getStatusLine().getStatusCode();
+
+                Log.d("---------><--------", "Antes!!!!!!!");
+                if (status == 200) {
+                    HttpEntity entity = response.getEntity();
+                    Log.d("---------><--------", "1!!!!!!!");
+                    String data = EntityUtils.toString(entity);
+                    Log.d("---------><--------", "2!!!!!!!");
+
+                    JSONObject jsono = new JSONObject(data);
+                    Log.d("---------><--------", "3!!!!!!!");
+
+                    rec = jsono.getString("name");
+                    Log.d("---------><--------", "4!!!!!!!");
+                    Log.d("---------><--------", "Depois!!!!!!!");
+
+                    return true;
+                }
+                Log.d("---------><--------", "--------------!!!!!!!");
+
+                //------------------>>
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+        protected void onPostExecute(Boolean result) {
+            dialog.cancel();
+            //notifyAll();
+            /*adapter.notifyDataSetChanged();
+            if(result == false)
+                Toast.makeText(getApplicationContext(), "Unable to fetch data from server", Toast.LENGTH_LONG).show();*/
+            getView().post(new Runnable() {
+                @Override
+                public void run() {
+                    ((TextView)(getView().findViewById(R.id.nomeId))).setText(rec);
+                }
+            });
+        }
     }
 }
