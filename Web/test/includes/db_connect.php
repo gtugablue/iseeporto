e9ac669dfdf166de1dbf0fbb8d4de5e9c4877462
gtugablue <gtugablue@gmail.com>
2015-09-11 18:32:54 +0100

    Deleted test directory.

--- Web/test/includes/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-
-if($db_connection->connect_error)
-{
-    die("$db_connection->connect_errno: $db_connection->connect_error");
-}
-?>
--- Web/test/includes/db_disconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Gustavo
- * Date: 08/09/2015
- * Time: 10:35
- */
-$db_connection->close();
-?>
