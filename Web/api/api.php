d05b55d3abb927df7e2782facc6e5bf234d1b44e
gtugablue <gtugablue@gmail.com>
2015-09-12 13:12:40 +0100

    Fixed API bug.

--- Web/api/api.php
+++ Web/api/api.php
@@ -433,7 +433,7 @@ $value = "An error has occurred";
 
 if (isset($_GET["action"]))
 {
-    if (isset($_SESSION["facebook_access_token"])) echo "Access Token: ".$_SESSION["facebook_access_token"];
+    //if (isset($_SESSION["facebook_access_token"])) echo "Access Token: ".$_SESSION["facebook_access_token"];
     switch (strtolower($_GET["action"]))
     {
         case "get_reviews":
